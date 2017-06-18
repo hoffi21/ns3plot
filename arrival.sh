5097e023a091dc37fd23c14a460c6d328821542f
Mario Hoffmann <s122114@hft-leipzig.de>
2017-06-18 11:57:38 -0700

    additional script to create arrival curves
    .you need 3 pcap's
    .no exception handling yet

---
+++ arrival.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+path="graphs/Arrival_Curves/"
+start=0
+end=710
+dataformat=png
+kind="arrivalcurve"
+j=0
+args_num="$#"
+ARG1=$1
+ARG2=$2
+ARG3=$3
+
+for i in "$@";
+    do
+    j=$((j+1))
+    echo '>>>> shark'
+    tshark -r $i -qz io,stat,1,tcp.stream==1,ip.src==10.1.2.1 -qz io,stat,1,tcp.stream==0,ip.src==10.1.1.1 | awk -v RS="" '{ print $0 > ".temp" NR }'
+    echo '>>>> build'
+    paste .temp1 .temp2 > Arrival_TCP_IO_$j
+    echo '>>>> clean temp-files'
+    rm .temp*
+done 
+
+echo '>>>> merge'
+paste Arrival_TCP_IO_$((args_num-2)) Arrival_TCP_IO_$((args_num-1)) Arrival_TCP_IO_$args_num > Arrival_TCP_IO
+echo '>>>> clean for a greater good'
+for (( k=1; k<=$args_num; k++ ))
+do
+    rm Arrival_TCP_IO_$k
+done
+
+echo '>>>> plot'
+function plot {
+gnuplot <<EOF
+	set terminal $dataformat size 900,500
+	set output '$path$kind-${start}s-${end}s.$dataformat'
+	set datafile separator "|"
+	set xrange [$start:$end]
+	set yrange [0:9000]
+	set xlabel "Zeit(s)"
+	set ylabel "Byte/s"
+	set style line 1 lt 3 lc rgb "orange" lw 1
+	set style line 2 lt 3 lc 3
+    set style line 3 lt 4 lc 2
+	plot \
+	"<(awk 'NR>=13 && NR<=720' Arrival_TCP_IO)" using 0:(\$6+\$12) t '${ARG1}' with lines linestyle 1, \
+    "<(awk 'NR>=13 && NR<=720' Arrival_TCP_IO)" using 0:(\$18+\$24) t '${ARG2}' with lines linestyle 2, \
+    "<(awk 'NR>=13 && NR<=720' Arrival_TCP_IO)" using 0:(\$30+\$36) t '${ARG3}' with lines linestyle 3
+EOF
+echo ">>>> finished"
+}
+
+start=0  ; end=710; plot
+start=0  ; end=30 ; plot
+start=680; end=710; plot
+mv Arrival_TCP_IO $path$kind.TCP_IO
